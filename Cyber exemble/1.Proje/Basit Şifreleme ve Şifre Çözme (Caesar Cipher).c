34978dd937459734a70218dc991c3bfb80cfba78
Meyy165 <165565320+Meyy165@users.noreply.github.com>
2025-03-28 00:14:59 +0300

    Basit Şifreleme ve Şifre Çözme (Caesar Cipher).c

--- Cyber exemble/1.Proje/Basit Şifreleme ve Şifre Çözme (Caesar Cipher).c
+++ Cyber exemble/1.Proje/Basit Şifreleme ve Şifre Çözme (Caesar Cipher).c
@@ -2,51 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 int main(){
-	int kym,i,no; //kym:kaydýrma miktarý
+	int kym,i,no; //kym:kaydÃ½rma miktarÃ½
 	char mesaj[100],ymesaj[100];//yeni mesaj
 	printf("lutfen kaydirma miktarini giriniz: ");
 	scanf("%d",&kym);
 	printf("lutfen mesajinizi giriniz: ");
-	getchar();  // Yeni satýr karakterini temizle (scanf'den önce)
+	getchar();  // Yeni satÃ½r karakterini temizle (scanf'den Ã¶nce)
 	fgets(mesaj,100,stdin);
 	printf("Lutfen bir secenek giriniz\n1)Msajimi sifrele\n2)Mesajin sifresini coz\nGirilen sayi: ");
 	scanf("%d",&no);
 	switch(no){
 		case 1:
-			for(i=0;mesaj[i]!='\0';i++){
+		for(i=0;mesaj[i]!='\0';i++){
 			if(mesaj[i] >='A' && mesaj[i] <='Z'){
-				ymesaj[i]=(mesaj[i]-'A'+kym)%26+'A';
-			}
+			ymesaj[i]=(mesaj[i]-'A'+kym)%26+'A';
+		   	 }
 			else if(mesaj[i] >= 'a' && mesaj[i] <= 'z'){
-	            ymesaj[i]=((mesaj[i]-'a'+kym)%26)+'a';
-	        } 
-	        else{
+	            	ymesaj[i]=((mesaj[i]-'a'+kym)%26)+'a';
+	            	} 
+	        	else{
 	        	ymesaj[i]=mesaj[i];
-			}
+		    }
 		}	
 		ymesaj[i] = '\0';  // Null karakter ekle
-        printf("Sifreli mesaj: %s\n", ymesaj);
+       		printf("Sifreli mesaj: %s\n", ymesaj);
 		break;
 		
 		case 2:
-				for(i=0;mesaj[i]!='\0';i++){
+		for(i=0;mesaj[i]!='\0';i++){
 			if(mesaj[i] >='A' && mesaj[i] <='Z'){
-				ymesaj[i]=(mesaj[i]-'A'-kym+26)%26+'A';
-			}
+			ymesaj[i]=(mesaj[i]-'A'-kym+26)%26+'A';
+			 }
 			else if(mesaj[i] >= 'a' && mesaj[i] <= 'z'){
-	            ymesaj[i]=((mesaj[i]-'a'-kym+26)%26)+'a';
-	        } 
-	        else{
+	            	ymesaj[i]=((mesaj[i]-'a'-kym+26)%26)+'a';
+	        	} 
+	       		else{
 	        	ymesaj[i]=mesaj[i];
-			}
+		    }
 		}	
 		ymesaj[i] = '\0';  
-        printf("Cozulen mesaj: %s\n", ymesaj);
+       		printf("Cozulen mesaj: %s\n", ymesaj);
 		break;
 		
 		default:
-			printf("Girilen sayi geçersiz");
-			break;
+		   printf("Girilen sayi geÃ§ersiz");
+		   break;
 	}
 
 	return 0;
